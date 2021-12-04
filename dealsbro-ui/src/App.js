ce0ae1cfa2103abe4301cbfacbda5ae83a6298ee
kev-inc <kevinchanxy@gmail.com>
2021-12-04 17:02:10 +0000

    configure routes

--- dealsbro-ui/src/App.js
+++ dealsbro-ui/src/App.js
@@ -1,11 +1,20 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route
+  } from "react-router-dom";
 import './App.css';
 import MapView from './map/Map';
+import Swagger from "./admin/Swagger";
 
 function App() {
     return (
-        <div>
-            <MapView />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/admin" element={<Swagger />}/>
+                <Route path="/" element={<MapView />}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
